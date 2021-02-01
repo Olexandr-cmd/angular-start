3659104790daf5662709b5225dcd253d0aa36a55
SashaKruts <disaster1209>
2021-02-01 19:14:10 +0200

    lesson5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-start';
+
+  name = new FormControl('', [Validators.required, Validators.minLength(10), this.fuckValidator]);
+  age = new FormControl('', Validators.required);
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age
+  });
+
+  action(): void {
+    console.log(this.myForm);
+  }
+
+  fuckValidator(inputData: AbstractControl): any {
+    if (inputData.value.includes('fuck')) {
+      return {fuck: true, msg: 'fuck is presented'};
+    }
+    return null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
